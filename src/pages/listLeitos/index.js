fac8bbcc270a716989ed235800dfc3736971c57a
Felipe Delfim <lipedelfim@gmail.com>
2022-04-09 14:48:43 -0300

    FlatList

--- src/pages/listLeitos/index.js
+++ src/pages/listLeitos/index.js
@@ -1,70 +1,33 @@
 import React from 'react';
 import { Component } from 'react';
-import { render } from 'react-dom';
-import { Text, View, FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import styles from './style'
 import Leitos from './leito'
 
 
-// export default class ListLeitos extends Component {
-export default function ListLeitos() {
+export default class ListLeitos extends Component {
 
-    return (
-        <View>
-            <View style={styles.containerLeitos}>
-                <Leitos />
+    constructor(props) {
+        super(props);
+        this.state = {
+            leitos: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <FlatList
+                    data={this.state.leitos}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ }) =>
+                        <View style={styles.containerLeitos}>
+                            <Leitos />
+                        </View>
+                    }
+                />
             </View>
+        );
+    }
 
-            <View style={styles.containerLeitos}>
-                <Leitos />
-            </View>
-
-            <View style={styles.containerLeitos}>
-                <Leitos />
-            </View>
-
-            <View style={styles.containerLeitos}>
-                <Leitos />
-            </View>
-
-            <View style={styles.containerLeitos}>
-                <Leitos />
-            </View>
-            <View style={styles.containerLeitos}>
-                <Leitos />
-            </View>
-        </View>
-    );
-
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         leitos: [
-    //             { id: "1", nome: 'Leito 1' },
-    //             { id: "2", nome: 'Leito 2' },
-    //             { id: "3", nome: 'Leito 3' },
-    //             { id: "4", nome: 'Leito 4' },
-    //             { id: "5", nome: 'Leito 5' },
-    //             { id: "6", nome: 'Leito 6' },
-    //             { id: "7", nome: 'Leito 7' },
-    //             { id: "8", nome: 'Leito 8' },
-    //         ]
-    //     }
-    // }
-
-    // render() {
-    //     return (
-    //         <View style={styles.container}>
-    //             <FlatList
-    //                 data={this.state.leitos}
-    //                 keyExtractor={(item) => item.id}
-    //                 renderItem={({ item }) =>
-    //                     <View style={styles.containerLeitos}>
-    //                         <Text style={styles.text}> {item.nome}</Text>
-    //                     </View>
-    //                 }
-    //             />
-    //         </View>
-    //     );
-    // }
 }
